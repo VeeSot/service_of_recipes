a52a1496d65f99dc8456542ed0f7a38f1bc61014
Vee Sot <veesot@devdrive.ru>
2016-03-19 16:56:42 +0500

    Поиск по названиям на клиентскую сторону

---
+++ static/js/app.js
@@ -0,0 +1,16 @@
+angular.module('app', [])
+    .controller('recipes_controller', ['$scope', '$http', function ($scope, $http) {
+        $scope.query = '';
+        $scope.recipes = [];
+        $scope.search = function () {
+            $http.post('/search/', {query: $scope.query}).success(
+                function (response) {
+
+                    $scope.recipes = response;
+                })
+        };
+    }]).config(function ($httpProvider) {
+    //Чтоб проходили запросы с проверкой CSRF-защиты
+    $httpProvider.defaults.xsrfCookieName = 'csrftoken';
+    $httpProvider.defaults.xsrfHeaderName = 'X-CSRFToken';
+});
