88398f54d69c60c224d42484963c58279d537ea0
Vee Sot <veesot@devdrive.ru>
2016-03-19 12:14:13 +0500

    Fix page title

--- main/views.py
+++ main/views.py
@@ -1,10 +1,6 @@
 # -*- coding: utf-8 -*-
 from django.shortcuts import render
 
-from main.forms import SearchForm
-
 
 def index(request):
-    form = SearchForm()
-    context = {'form': form}
-    return render(request, 'index.html', context)
+    return render(request, 'index.html')
